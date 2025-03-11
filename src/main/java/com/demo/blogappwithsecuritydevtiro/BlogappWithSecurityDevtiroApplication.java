8b0172465f9c9959438cb3032feee9830bc1ce64
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-03-12 00:12:30 +0300

    Create Project

---
+++ src/main/java/com/demo/blogappwithsecuritydevtiro/BlogappWithSecurityDevtiroApplication.java
@@ -0,0 +1,13 @@
+package com.demo.blogappwithsecuritydevtiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogappWithSecurityDevtiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogappWithSecurityDevtiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/blogappwithsecuritydevtiro/BlogappWithSecurityDevtiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.blogappwithsecuritydevtiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogappWithSecurityDevtiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
