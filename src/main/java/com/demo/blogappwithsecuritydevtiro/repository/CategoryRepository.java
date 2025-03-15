20b5c653f83523a5824a22638cace354b182e0bc
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-03-16 01:43:29 +0300

    change id type from Long to UUID

--- src/main/java/com/demo/blogappwithsecuritydevtiro/repository/CategoryRepository.java
+++ src/main/java/com/demo/blogappwithsecuritydevtiro/repository/CategoryRepository.java
@@ -3,6 +3,8 @@ package com.demo.blogappwithsecuritydevtiro.repository;
 import com.demo.blogappwithsecuritydevtiro.domain.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository<Category, Long> {
+import java.util.UUID;
+
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
     boolean existsByName(String name);
 }
