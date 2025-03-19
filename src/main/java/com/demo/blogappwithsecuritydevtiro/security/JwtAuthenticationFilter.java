a1c9796045a19a27384b9fb190e63b14bd8b97e9
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-03-19 03:24:45 +0300

    create JwtAuthenticationFilter

---
+++ src/main/java/com/demo/blogappwithsecuritydevtiro/security/JwtAuthenticationFilter.java
@@ -0,0 +1,62 @@
+package com.demo.blogappwithsecuritydevtiro.security;
+
+import com.demo.blogappwithsecuritydevtiro.service.AuthenticationService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final AuthenticationService authenticationService;
+
+    public JwtAuthenticationFilter(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        try {
+            String token = extractToken(request);
+            if (token != null) {
+                UserDetails userDetails = authenticationService.validateToken(token);
+
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(
+                                userDetails,
+                                null,
+                                userDetails.getAuthorities()
+                        );
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+                if (userDetails instanceof CustomUserDetails) {
+                    request.setAttribute("userId", ((CustomUserDetails) userDetails).getUserId());
+                }
+
+            }
+        } catch (Exception e) {
+
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String extractToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+}
