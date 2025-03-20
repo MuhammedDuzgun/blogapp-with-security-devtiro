bf9996d33b3e678bcb27d65042d7fd6609584620
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-03-20 03:46:47 +0300

    add findByName method

--- src/main/java/com/demo/blogappwithsecuritydevtiro/repository/TagRepository.java
+++ src/main/java/com/demo/blogappwithsecuritydevtiro/repository/TagRepository.java
@@ -3,5 +3,11 @@ package com.demo.blogappwithsecuritydevtiro.repository;
 import com.demo.blogappwithsecuritydevtiro.domain.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TagRepository extends JpaRepository<Tag, Long> {
+import java.util.Optional;
+import java.util.UUID;
+
+public interface TagRepository extends JpaRepository<Tag, UUID> {
+
+    Optional<Tag> findByName(String name);
+
 }
