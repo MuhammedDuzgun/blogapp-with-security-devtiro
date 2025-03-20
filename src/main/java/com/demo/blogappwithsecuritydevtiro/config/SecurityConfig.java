c62b812880b012674e8b5154a7d9df7436123bf4
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-03-20 03:47:14 +0300

    modify SecurityConfig

--- src/main/java/com/demo/blogappwithsecuritydevtiro/config/SecurityConfig.java
+++ src/main/java/com/demo/blogappwithsecuritydevtiro/config/SecurityConfig.java
@@ -1,5 +1,11 @@
 package com.demo.blogappwithsecuritydevtiro.config;
 
+import com.demo.blogappwithsecuritydevtiro.domain.entity.User;
+import com.demo.blogappwithsecuritydevtiro.repository.UserRepository;
+import com.demo.blogappwithsecuritydevtiro.security.CustomUserDetailsService;
+import com.demo.blogappwithsecuritydevtiro.security.JwtAuthenticationFilter;
+import com.demo.blogappwithsecuritydevtiro.service.AuthenticationService;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,26 +14,49 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public JwtAuthenticationFilter jwtAuthenticationFilter(AuthenticationService authenticationService) {
+        return new JwtAuthenticationFilter(authenticationService);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        CustomUserDetailsService customUserDetailsService = new CustomUserDetailsService(userRepository);
+        String email = "user@test.com";
+        userRepository.findByEmail(email).orElseGet(()-> {
+            User user = new User();
+            user.setName("test user");
+            user.setEmail(email);
+            user.setPassword(passwordEncoder().encode("password"));
+            return userRepository.save(user);
+        });
+        return customUserDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,
+                                                   JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/api/v1/auth").permitAll()
                         .requestMatchers(HttpMethod.GET, "api/v1/posts").permitAll()
                         .requestMatchers(HttpMethod.GET, "api/v1/categories").permitAll()
-                        .requestMatchers(HttpMethod.GET, "api/v1/tags").permitAll()
-                        .anyRequest().authenticated()
+                        .requestMatchers("/api/v1/tags").permitAll()
+                        .anyRequest().permitAll()
                 )
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session ->
                     session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                );
+                ).addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
